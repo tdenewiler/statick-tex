02e8baf45fee0878954575894fe40a22c8dbe51f
Thomas Denewiler <tdenewiler@gmail.com>
2019-10-28 19:52:07 -0700

    Basics of source layout are working. Some unit tests do not pass. Need to test installed version puts files in correct spots.

--- setup.py
+++ setup.py
@@ -1,35 +1,26 @@
 """Setup."""
 
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md') as f:
     long_description = f.read()  # pylint: disable=invalid-name
 
 TEST_DEPS = [
-    'pytest',
     'mock',
+    'pytest',
 ]
 
 EXTRAS = {
     'test': TEST_DEPS,
 }
 
-VERSION = '0.1.2'
-
 setup(
-    author='SSC Pacific',
-    name='statick-tex',
-    description='Tool for running static analysis tools against TeX/LaTeX.',
-    version=VERSION,
-    packages=['statick_tool',
-              'statick_tool.plugins.discovery',
-              'statick_tool.plugins.tool'],
-    package_dir={'statick_tool': '.',
-                 'statick_tool.plugins.discovery':
-                 'plugins/tex_discovery_plugin',
-                 'statick_tool.plugins.tool':
-                 'plugins/tex_tool_plugins'},
+    author='Thomas Denewiler',
+    name='statick_tool',
+    description='Statick analysis plugins for TeX/LaTeX files and projects.',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     package_data={'statick_tool': ['rsc/*'],
                   'statick_tool.plugins.discovery':
                   ['*.yapsy-plugin'],
@@ -48,6 +39,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Testing",
     ],
 )
