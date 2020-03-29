37a576396e80d73b16d63d1ae7f00de9d68b25c0
Thomas Denewiler <tdenewiler@gmail.com>
2020-03-29 08:33:43 -0700

    Fixing setup.py to work on Windows.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description='Statick analysis plugins for TeX/LaTeX files and projects.',
     version='0.2.0',
     packages=['statick_tool', 'statick_tool.plugins.discovery', 'statick_tool.plugins.tool'],
-    package_dir={'statick_tool.plugins.discovery': 'src/statick_tex/plugins/discovery/',
+    package_dir={'statick_tool.plugins.discovery': 'src/statick_tex/plugins/discovery',
                  'statick_tool.plugins.tool': 'src/statick_tex/plugins/tool',
                  'statick_tool': '.'},
     package_data={'statick_tool': ['rsc/*'],
