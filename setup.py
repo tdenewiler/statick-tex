805903d84365be7947529154bf9bfbfb28bfaec1
Thomas Denewiler <tdenewiler@gmail.com>
2021-09-08 14:27:45 -0700

    Remove support for Ubuntu 16.04 and Python 3.5. Fix pylint warnings related to using the open call without specifying an encoding. (#54)

--- setup.py
+++ setup.py
@@ -3,40 +3,47 @@
 
 from setuptools import setup
 
-with open('README.md') as f:
-    long_description = f.read()  # pylint: disable=invalid-name
+with open("README.md", encoding="utf-8") as fid:
+    long_description = fid.read()  # pylint: disable=invalid-name
 
 TEST_DEPS = [
-    'mock',
-    'pre-commit',
-    'pytest',
+    "mock",
+    "pre-commit",
+    "pytest",
 ]
 
 EXTRAS = {
-    'test': TEST_DEPS,
+    "test": TEST_DEPS,
 }
 
 setup(
-    author='Thomas Denewiler',
-    name='statick-tex',
-    description='Statick analysis plugins for TeX/LaTeX files and projects.',
-    version='0.2.4',
-    packages=['statick_tool', 'statick_tool.plugins.discovery', 'statick_tool.plugins.tool'],
-    package_dir={'statick_tool.plugins.discovery': 'src/statick_tex/plugins/discovery',
-                 'statick_tool.plugins.tool': 'src/statick_tex/plugins/tool',
-                 'statick_tool': '.'},
-    package_data={'statick_tool': ['rsc/*'],
-                  'statick_tool.plugins.discovery': ['*.yapsy-plugin'],
-                  'statick_tool.plugins.tool':['*.yapsy-plugin']},
+    author="Thomas Denewiler",
+    name="statick-tex",
+    description="Statick analysis plugins for TeX/LaTeX files and projects.",
+    version="0.2.4",
+    packages=[
+        "statick_tool",
+        "statick_tool.plugins.discovery",
+        "statick_tool.plugins.tool",
+    ],
+    package_dir={
+        "statick_tool.plugins.discovery": "src/statick_tex/plugins/discovery",
+        "statick_tool.plugins.tool": "src/statick_tex/plugins/tool",
+        "statick_tool": ".",
+    },
+    package_data={
+        "statick_tool": ["rsc/*"],
+        "statick_tool.plugins.discovery": ["*.yapsy-plugin"],
+        "statick_tool.plugins.tool": ["*.yapsy-plugin"],
+    },
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=['statick'],
+    long_description_content_type="text/markdown",
+    install_requires=["statick"],
     tests_require=TEST_DEPS,
     extras_require=EXTRAS,
-    url='https://github.com/tdenewiler/statick-tex',
+    url="https://github.com/tdenewiler/statick-tex",
     classifiers=[
         "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
